ea2b82f86bdfecd75a6495c053d5c87bc29e6859
vyomshah14 <vyomshah2021@gmail.com>
2026-01-07 13:08:20 +0530

    Learning and trying the Conditions is JS

---
+++ Day-3/condition.js
@@ -0,0 +1,20 @@
+//condition
+if (0) console.log("inside if");
+// with if else statement
+if (1){
+    console.log("true");
+}else {
+    console.log("false");
+}
+// with condition ? and :
+(true) ? console.log(1) : console.log(0);
+
+// with if else statement age verification
+let age = 18;
+if (age < 18) {
+    console.log("Age is less than 18");
+}else if (age === 18) {
+    console.log("Age is 18");
+}else {
+    console.log("Age is verifying");
+}
