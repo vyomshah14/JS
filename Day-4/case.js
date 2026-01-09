5c3ef924eac33b45cc479bc91e4767d30ce977ff
vyomshah14 <vyomshah2021@gmail.com>
2026-01-09 10:41:41 +0530

    modified

---
+++ Day-4/case.js
@@ -0,0 +1,13 @@
+// switch case
+let age = 20;
+switch (age){
+    case (age < 18):
+        console.log("You are a minor.");
+    case (age == 18):
+        console.log("You are 18.");
+    case (age >18):
+        console.log("You are an adult.");
+        break;
+    default:
+        console.log("enter number properly");
+}
